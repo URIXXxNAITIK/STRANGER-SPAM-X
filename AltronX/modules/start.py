d81f7c0d7376a2d9b7a45b4cb1f80abd808c3ba8
⪻乂  ᑌ ℞ Ꭵ ᙭ ᙭ ꗃ 乂⪼⇝⎝ツ⎠﹃Ό <155140402+URIXXxNAITIK@users.noreply.github.com>
2024-01-16 16:36:27 +0530

    Update start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -8,11 +8,11 @@ PythonButton = [
         Button.inline("☆ 𝐂σммαиԃ𝐒 ☆", data="help_back")
         ],
         [
-        Button.url("☆ 𝐂нαииє𝐋 ☆", "https://t.me/SHIVANSH474"),
-        Button.url("☆ 𝐒υρρσя𝐓", "https://t.me/mastiwithfriendsx")
+        Button.url("☆ 𝐂нαииє𝐋 ☆", "https://t.me/ll4st_opII"),
+        Button.url("☆ 𝐒υρρσя𝐓", "https://t.me/II_4ST_FIGHTER_ll")
         ],
         [
-        Button.url("☆ 𝐒𝐇𝐈𝐕𝐀𝐍𝐒𝐇-𝐗𝐃 ☆", "https://t.me/SHIVANSH39")
+        Button.url("☆ 4ꜱᴛ-𝐗𝐃 ☆", "https://t.me/II_4ST_BALE_II")
         ]
         ]
 
@@ -34,8 +34,8 @@ async def start(event):
         BotName = AltBot.first_name
         BotId = AltBot.id
         TEXT = f"**𝐇𝐞𝐲 [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝐈 𝐀𝐦 [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **✦ 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐃 𝐁𝐘 :~ [𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥](https://t.me/SHIVANSH39)**\n\n"
-        TEXT += f"» **𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑 𝐒𝐏𝐀𝐌 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 :** `3.2`\n"
+        TEXT += f"» **✦ 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐃 𝐁𝐘 :~ [𝐂𝐑𝐀𝐙𝐘 𝐔𝐑𝐈𝐗𝐗](https://t.me/SHIVANSH39)**\n\n"
+        TEXT += f"» **4ꜱᴛ 𝐒𝐏𝐀𝐌 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 :** `3.2`\n"
         TEXT += f"» **𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍:** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                 event.chat_id,
